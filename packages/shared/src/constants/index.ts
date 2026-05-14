169017f3a629a6d16e65ac32d6ce0e6ddc0b7174
juno-shon <sanolx30@gmail.com>
2026-05-14 18:09:54 +0900

    fix: configure cloudfront webview deployment

--- packages/shared/src/constants/index.ts
+++ packages/shared/src/constants/index.ts
@@ -1,5 +1,5 @@
 export const WEB_APP_DEV_URL = 'http://localhost:5173';
-export const WEB_APP_PROD_URL = 'https://errander.app';
+export const WEB_APP_PROD_URL = 'https://d1qfzqy0j1ja2j.cloudfront.net';
 
 export const WEBVIEW_MESSAGE_TIMEOUT_MS = 5000;
 export const QUERY_STALE_TIME_MS = 60_000;
